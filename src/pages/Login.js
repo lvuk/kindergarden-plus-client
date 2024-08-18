b2624d679b365ee44e7539a08733c3c1152981e4
lvuk <luka.vuk456@gmail.com>
2024-08-18 18:16:18 +0200

    login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import '../stylesheets/login.scss';
+import Header from '../components/Header';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <Header />
+      <div className='login'>
+        <div className='container'>
+          <h1>Kindergarden+</h1>
+          <h2>Login</h2>
+          <form action='' className='form'>
+            <input type='text' placeholder='email' />
+            <input type='password' placeholder='password' />
+            <button className='btn'>Login</button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
