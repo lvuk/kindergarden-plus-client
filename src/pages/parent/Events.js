0a2481d7daf9b89a4d450e36a2461cec1e82cb1a
lvuk <luka.vuk456@gmail.com>
2024-09-17 12:03:01 +0200

    Events calendar added

--- src/pages/parent/Events.js
+++ src/pages/parent/Events.js
@@ -1,7 +1,54 @@
-import React from 'react';
+/* eslint-disable no-undef */
+import '../../stylesheets/parent/events.scss';
+import React, { useState } from 'react';
+// import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import FullCalendar from '@fullcalendar/react';
+import dayGrid from '@fullcalendar/daygrid';
+import timeGrid from '@fullcalendar/timegrid';
+
+// const localizer = momentLocalizer(moment);
 
 const Events = () => {
-  return <div>Events</div>;
+  const [events, setEvents] = useState([
+    {
+      id: 1,
+      title: 'Some event with realllyyy longg ggtittlltleee',
+      start: '2024-09-17',
+      end: '2024-09-19',
+    },
+  ]);
+
+  return (
+    <div className='events'>
+      <FullCalendar
+        plugins={[timeGrid, dayGrid]}
+        initialView='dayGridMonth'
+        fixedWeekCount={false}
+        headerToolbar={{
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek',
+        }}
+        height={'90vh'}
+        firstDay={1}
+        nowIndicator={true}
+        now={moment().format()}
+        slotLabelFormat={{
+          hour: 'numeric',
+          minute: '2-digit',
+          omitZeroMinute: false,
+          meridiem: 'lowercase',
+          hour12: false,
+        }}
+        slotMinTime={'06:00:00'}
+        events={events}
+        eventClick={(info) => {
+          alert('Event: ' + info.event.title);
+        }}
+      />
+    </div>
+  );
 };
 
 export default Events;
