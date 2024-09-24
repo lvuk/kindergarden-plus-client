31e48dfb128d450b22adf1e8af3487b97ca7cbc8
lvuk <luka.vuk456@gmail.com>
2024-09-24 09:53:56 +0200

    mini profile visual fix

--- src/pages/parent/ChildProfile.js
+++ src/pages/parent/ChildProfile.js
@@ -15,7 +15,7 @@ const ChildProfile = () => {
         </div>
       </div>
       <div className='child-profile-body'>
-        <div>
+        <div className='child-profile-container'>
           <h2>Basic Info</h2>
           <p>Group: Zelena grupa (Predskolski)</p>
           <p>Teachers: JJ & KK</p>
@@ -24,21 +24,20 @@ const ChildProfile = () => {
           <p>PIN: 506644029212</p>
           <p>Address: Test address</p>
         </div>
-        <div>
+        <div className='child-profile-container'>
           <h2>Health Record</h2>
           <p>Height: 100cm</p>
           <p>Weight: 20kg</p>
           <p>Shoe size: 30</p>
           <p>Health issues: None</p>
         </div>
-        <div>
+        <div className='child-profile-container'>
           <h2>Attendance</h2>
           <p>This week: 100%</p>
           <p>This month: 95%</p>
           <p>This year: 93%</p>
         </div>
       </div>
-      <Hr style={{ width: '80%' }} />
     </div>
   );
 };
