730bc37d7d64189ea963e41a94b75530c3696d9c
lvuk <luka.vuk456@gmail.com>
2024-10-16 12:25:49 +0200

    added form on children part

--- src/pages/manager/Children.js
+++ src/pages/manager/Children.js
@@ -1,14 +1,18 @@
-import { Button, Input, Modal, Space, Table, Tag } from 'antd';
+import { Button, Form, Input, Modal, Select, Space, Table, Tag } from 'antd';
 import React, { useContext, useEffect, useRef, useState } from 'react';
 import { SearchOutlined } from '@ant-design/icons';
 import Highlighter from 'react-highlight-words';
 import { LayoutContext } from '../context/LayoutContext';
+import { Option } from 'antd/es/mentions';
+import DebounceSelect from '../../components/manager/DebounceSelect';
 
 const Children = () => {
   const [searchText, setSearchText] = useState('');
   const [searchedColumn, setSearchedColumn] = useState('');
+  const [value, setValue] = useState([]);
   const { isModalOpen, setIsModalOpen, activeTab } = useContext(LayoutContext);
   const searchInput = useRef(null);
+  const [form] = Form.useForm();
 
   useEffect(() => {
     setIsModalOpen(false);
@@ -176,6 +180,42 @@ const Children = () => {
 
   const handleCancel = () => {
     setIsModalOpen(false);
+    form.resetFields();
+  };
+
+  const handleSubmit = (values) => {
+    console.log(values);
+    // setIsModalOpen(false);
+  };
+
+  const fetchUserList = (lastName) => {
+    return fetch('https://randomuser.me/api/?results=5')
+      .then((response) => response.json())
+      .then((body) =>
+        body.results.map((user) => ({
+          label: `${user.name.first} ${user.name.last}`,
+          value: user.login.uuid,
+        }))
+      );
+  };
+
+  const formItemLayout = {
+    labelCol: {
+      xs: {
+        span: 24,
+      },
+      sm: {
+        span: 6,
+      },
+    },
+    wrapperCol: {
+      xs: {
+        span: 24,
+      },
+      sm: {
+        span: 14,
+      },
+    },
   };
 
   return (
@@ -187,8 +227,92 @@ const Children = () => {
           title='Add new child'
           onCancel={handleCancel}
           footer={null}
+          maskClosable={false}
         >
-          <p>CHILDREN</p>
+          <Form
+            {...formItemLayout}
+            variant='filled'
+            onFinish={handleSubmit}
+            form={form}
+          >
+            <Form.Item
+              label='First Name'
+              name='firstName'
+              rules={[
+                {
+                  required: true,
+                  message: "Can't be empty!",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label='Last Name'
+              name='lastName'
+              rules={[
+                {
+                  required: true,
+                  message: "Can't be empty!",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label='PIN'
+              name='PIN'
+              rules={[
+                {
+                  required: true,
+                },
+                {
+                  pattern: /^[0-9]{11}$/,
+                  message: 'PIN must have 11 digits',
+                },
+              ]}
+            >
+              <Input type='' />
+            </Form.Item>
+
+            <Form.Item
+              name='parents'
+              label='Parents'
+              rules={[
+                {
+                  required: true,
+                  message: 'Please select at least one parent',
+                  type: 'array',
+                },
+              ]}
+            >
+              <DebounceSelect
+                mode='multiple'
+                value={value}
+                placeholder='Select parents'
+                fetchOptions={fetchUserList}
+                onChange={(newValue) => {
+                  setValue(newValue);
+                }}
+                style={{
+                  width: '100%',
+                }}
+              />
+            </Form.Item>
+
+            <Form.Item
+              wrapperCol={{
+                offset: 6,
+                span: 14,
+              }}
+            >
+              <Button type='primary' htmlType='submit'>
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
         </Modal>
       )}
     </div>
